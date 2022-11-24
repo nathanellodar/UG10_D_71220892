77bbafcafac8af58dfd7bfc5f76da65e6716bf89
nathanellodar <117901115+nathanellodar@users.noreply.github.com>
2022-11-24 16:16:50 +0700

    Add files via upload

---
+++ 1_D_71220892.py
@@ -0,0 +1,22 @@
+print("\(^0^) Selamat datang di kalkulator sederhana(^0^)/")
+print("Ketik 1 untuk menghitung penjumlahan")
+print("Ketik 2 untuk menghitung pengurangan")
+print("Ketik 3 untuk menghitung perkalian")
+print("Ketik 4 untuk menghitung pembagian")
+print("Ketik 5 untuk menghitung sisa hasil bagi (modulus)")
+print("Ketik 6 untuk menghitung pemangkatan")
+pilihan = 1
+while pilihan <=5:
+    pilihan = int(input("masukan pilhan anda:"))
+    if pilihan == 1:
+        p=int(input("Masukan bilangan pertama:"))
+        d=int(input("Masukan bilangan kedua:"))
+        rumus1=(p+d)
+        print("hasil dari", p, "di jumlahkan dengan", d, "adalah", rumus1)
+        
+    elif pilihan == 2:
+        p=int(input("Masukan bilangan pertama:"))
+        d=int(input("Masukan bilangan kedua:"))
+        rumus2=(p-d)
+        print("hasil dari", p, "di pengurangan dengan", d, "adalah", rumus2)
+    
