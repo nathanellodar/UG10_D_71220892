b00ec44204614edebc8b10de65c26f119dbd516d
nathanellodar <117901115+nathanellodar@users.noreply.github.com>
2022-11-24 16:34:59 +0700

    Add files via upload

---
+++ 2_D_71220892.py
@@ -0,0 +1,19 @@
+n1 = input("Masukan nama pemain pertama: ")
+n2 = input("Masukan nama pemain kedua: ")
+n3 = input("Masukan nama pemain ketiga: ")
+a1 = int(input("Masukan jumlah kartu pemain pertama: "))
+a2 = int(input("Masukan jumlah kartu pemain kedua: "))
+a3 = int(input("Masukan jumlah kartu pemain ketiga: "))
+
+if (a1>a2>a3):
+    print(n1, "menang dengan jumlah kartu sebanyak", a1)
+elif (a2>a1>a3):
+    print(n2, "menang dengan jumlah kartu sebanyak", a2)
+elif (a3>a1>a2):
+    print(n3, "menang dengan jumlah kartu sebanyak", a3)
+elif (a1>21):
+    print("Jumlah kartu yang dimiliki melebihi batas")
+elif (a2>21):
+    print("Jumlah kartu yang dimiliki melebihi batas")
+elif (a3>21):
+    print("Jumlah kartu yang dimiliki melebihi batas")
