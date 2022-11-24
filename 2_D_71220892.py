0bb2f9ac9d6ba0f5b10a0d9af16448e7295c4b04
nathanellodar <117901115+nathanellodar@users.noreply.github.com>
2022-11-24 17:08:29 +0700

    Update 2_D_71220892.py

--- 2_D_71220892.py
+++ 2_D_71220892.py
@@ -1,19 +1,19 @@
-n1 = input("Masukan nama pemain pertama: ")
-n2 = input("Masukan nama pemain kedua: ")
-n3 = input("Masukan nama pemain ketiga: ")
-a1 = int(input("Masukan jumlah kartu pemain pertama: "))
-a2 = int(input("Masukan jumlah kartu pemain kedua: "))
-a3 = int(input("Masukan jumlah kartu pemain ketiga: "))
+pertama = input("Masukan nama pemain pertama: ")
+kedua = input("Masukan nama pemain kedua: ")
+ketiga = input("Masukan nama pemain ketiga: ")
+in1 = int(input("Masukan jumlah kartu pemain pertama: "))
+in2 = int(input("Masukan jumlah kartu pemain kedua: "))
+in3 = int(input("Masukan jumlah kartu pemain ketiga: "))
 
-if (a1>a2 and a3):
-    print(n1, "menang dengan jumlah kartu sebanyak", a1)
-elif (a2>a1 and a3):
-    print(n2, "menang dengan jumlah kartu sebanyak", a2)
-elif (a3>a1 and a2):
-    print(n3, "menang dengan jumlah kartu sebanyak", a3)
-elif (a1>21):
+if (in1> in2 and in3):
+    print(pertama, "menang dengan jumlah kartu sebanyak", in1)
+elif (in2> in1 and in3):
+    print(kedua, "menang dengan jumlah kartu sebanyak", in2)
+elif (in3> in1 and in3):
+    print(ketiga, "menang dengan jumlah kartu sebanyak", in3)
+elif (in1>21):
     print("Jumlah kartu yang dimiliki melebihi batas")
-elif (a2>21):
+elif (in2>21):
     print("Jumlah kartu yang dimiliki melebihi batas")
-elif (a3>21):
+elif (in3>21):
     print("Jumlah kartu yang dimiliki melebihi batas")
